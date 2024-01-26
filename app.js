1a0081f00659e6ab72d9a982bd565aa0682972c0
Mufamadi Anwani Nicolas <mufamadianwani@gmail.com>
2024-01-26 11:28:24 +0200

    Updated hello world msg

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const http = require('http')
 
 http.createServer(function(req,res) {
     res.writeHead(200,{'Content-Type': 'text/plain'})
-    res.write('Hello World')
+    res.write(`Hello World from:
+    ___  __  ____  ____             
+   / __)/  \(    \(  __)            
+  ( (__(  O )) D ( ) _)             
+   \___)\__/(____/(____)            
+____  __ _   ___  __  __ _  ____ 
+(  __)(  ( \ / __)(  )(  ( \(  __)
+) _) /    /( (_ \ )( /    / ) _) 
+(____)\_)__) \___/(__)\_)__)(____)`)
     res.end()
 }).listen(8000,'localhost',()=>{
     console.log('Server running')
